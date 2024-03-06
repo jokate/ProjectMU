7be48eb66b033c677be703a3f6e55231747e2a36
jokate <kkkk4017@naver.com>
2024-03-06 10:01:59 +0900

    MU_2024-03-06/10:00
    데이터를 기반으로 하여 Interaction 정보를 출력할 수 있도록 데이터 구성 완료.

---
+++ MoonU/Public/Data/DataTable/MUWidgetData.h
@@ -0,0 +1,22 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayTagContainer.h"
+#include "Engine/DataTable.h"
+#include "UObject/Object.h"
+#include "MUWidgetData.generated.h"
+
+
+USTRUCT(BlueprintType)
+struct FInteractionBoardData : public FTableRowBase
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FGameplayTag InteractionTag;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FText InteractionText;
+};
