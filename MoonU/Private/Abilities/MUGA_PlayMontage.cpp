3d0264d6e4b8e510b14ea7ae45537367344b0fb5
jokate <kkkk4017@naver.com>
2024-04-10 19:04:21 +0900

    MU-2024-04-10 몽타주 재생 로직 변경 (Wait에서 그냥 재생으로 변경)

--- MoonU/Private/Abilities/MUGA_PlayMontage.cpp
+++ MoonU/Private/Abilities/MUGA_PlayMontage.cpp
@@ -3,7 +3,11 @@
 
 #include "Abilities/MUGA_PlayMontage.h"
 
+#include "AbilitySystemBlueprintLibrary.h"
+#include "AbilitySystemComponent.h"
 #include "Abilities/Tasks/AbilityTask_PlayMontageAndWait.h"
+#include "GameFramework/Character.h"
+#include "GameFramework/CharacterMovementComponent.h"
 
 
 UMUGA_PlayMontage::UMUGA_PlayMontage()
@@ -15,14 +19,20 @@ void UMUGA_PlayMontage::ActivateAbility(const FGameplayAbilitySpecHandle Handle,
                                   const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData)
 {
 	Super::ActivateAbility(Handle, ActorInfo, ActivationInfo, TriggerEventData);
+	
+	ACharacter* Character = Cast<ACharacter>(ActorInfo->AvatarActor.Get());
 
-	UAbilityTask_PlayMontageAndWait* NewTask = UAbilityTask_PlayMontageAndWait::CreatePlayMontageAndWaitProxy(this, TEXT("OnHit"), MontageToPlay, 1.0f);
-	NewTask->OnCompleted.AddDynamic(this, &UMUGA_PlayMontage::OnCompleteCallback);
-	NewTask->OnInterrupted.AddDynamic(this, &UMUGA_PlayMontage::OnInterruptedCallback);
-	NewTask->OnCancelled.AddDynamic(this, &UMUGA_PlayMontage::OnInterruptedCallback);
-	NewTask->OnBlendOut.AddDynamic(this, &UMUGA_PlayMontage::OnInterruptedCallback);
+	if (!Character)
+	{
+		return;
+	}
 
-	NewTask->ReadyForActivation();
+	Character->PlayAnimMontage(MontageToPlay);
+	
+	//명시적으로 Ability가 끝났음을 알림
+	bool bReplicatedEndAbility = true;
+	bool bWasCancelled = true;
+	EndAbility(CurrentSpecHandle, CurrentActorInfo, CurrentActivationInfo, bReplicatedEndAbility, bWasCancelled);
 }
 
 void UMUGA_PlayMontage::OnCompleteCallback()
