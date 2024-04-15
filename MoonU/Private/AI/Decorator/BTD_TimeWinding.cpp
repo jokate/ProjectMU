6ed7ce480fcac90d155e9e6810abea05a921860d
jokate <kkkk4017@naver.com>
2024-04-15 16:20:55 +0900

    MU_2024-04-15/16:20 데코레이터 추가 완료.

---
+++ MoonU/Private/AI/Decorator/BTD_TimeWinding.cpp
@@ -0,0 +1,26 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AI/Decorator/BTD_TimeWinding.h"
+
+#include "AIController.h"
+#include "Interface/TimerWindTarget.h"
+
+bool UBTD_TimeWinding::CalculateRawConditionValue(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) const
+{
+	APawn* Pawn = OwnerComp.GetAIOwner()->GetPawn();
+
+	if (!Pawn)
+	{
+		return false;
+	}
+
+	ITimeWindTarget* Target = Cast<ITimeWindTarget>(Pawn);
+
+	if (Target == nullptr)
+	{
+		return false;
+	}
+	
+	return Target->GetTimeWind();
+}
---
+++ MoonU/Public/AI/Decorator/BTD_TimeWinding.h
@@ -0,0 +1,19 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/BTDecorator.h"
+#include "BTD_TimeWinding.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MOONU_API UBTD_TimeWinding : public UBTDecorator
+{
+	GENERATED_BODY()
+
+	virtual bool CalculateRawConditionValue(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) const;
+	
+};
