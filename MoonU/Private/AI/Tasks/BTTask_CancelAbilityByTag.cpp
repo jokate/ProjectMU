76cb75b238a5433e4fb87205e21f1ec9facf640d
jokate <kkkk4017@naver.com>
2024-04-27 18:44:01 +0900

    MU_2024-04-27 / 18 : 43 필요성에 의한 CancelAbility 생성.

---
+++ MoonU/Private/AI/Tasks/BTTask_CancelAbilityByTag.cpp
@@ -0,0 +1,36 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AI/Tasks/BTTask_CancelAbilityByTag.h"
+
+#include "AbilitySystemBlueprintLibrary.h"
+#include "AbilitySystemComponent.h"
+#include "AIController.h"
+
+EBTNodeResult::Type UBTTask_CancelAbilityByTag::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
+{
+	AAIController* AIController = OwnerComp.GetAIOwner();
+
+	if (AIController == nullptr)
+	{
+		return EBTNodeResult::Failed;
+	}
+
+	APawn* Pawn = AIController->GetPawn();
+
+	if (Pawn == nullptr)
+	{
+		return EBTNodeResult::Failed;
+	}
+
+	UAbilitySystemComponent* ASC = UAbilitySystemBlueprintLibrary::GetAbilitySystemComponent(Pawn);
+
+	if (ASC == nullptr)
+	{
+		return EBTNodeResult::Failed;
+	}
+
+	ASC->CancelAbilities(&NeedToCancelAbilityTags, nullptr);
+
+	return EBTNodeResult::Succeeded;
+}
---
+++ MoonU/Public/AI/Tasks/BTTask_CancelAbilityByTag.h
@@ -0,0 +1,21 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/BTTaskNode.h"
+#include "BTTask_CancelAbilityByTag.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MOONU_API UBTTask_CancelAbilityByTag : public UBTTaskNode
+{
+	GENERATED_BODY()
+
+	virtual EBTNodeResult::Type ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) override;
+public :
+	UPROPERTY(EditAnywhere)
+	FGameplayTagContainer NeedToCancelAbilityTags;
+};
