6adf3b3357527e4a055a2d3708922465a43e7da1
jokate <kkkk4017@naver.com>
2024-04-27 18:13:12 +0900

    MU_2024-04-27 / 18 : 13 오버 엔지니어링 코드 업로드

---
+++ MoonU/Private/AI/Service/BTS_SetTimerForChangeValue.cpp
@@ -0,0 +1,78 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AI/Service/BTS_SetTimerForChangeValue.h"
+
+#include "AIController.h"
+#include "BehaviorTree/BlackboardComponent.h"
+
+UBTS_SetTimerForChangeValue::UBTS_SetTimerForChangeValue()
+{
+	bNotifyBecomeRelevant = true;
+
+	bNotifyCeaseRelevant = true;
+}
+
+void UBTS_SetTimerForChangeValue::OnBecomeRelevant(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
+{
+	Super::OnBecomeRelevant(OwnerComp, NodeMemory);
+	OnTimerEnd.BindUObject(this, &UBTS_SetTimerForChangeValue::SetTimerForChangeValue, TWeakObjectPtr<UBehaviorTreeComponent>(&OwnerComp));
+	GetWorld()->GetTimerManager().SetTimer(ChangeTimer, this, &UBTS_SetTimerForChangeValue::BroadcastEvent, TimerInterval);
+}
+
+void UBTS_SetTimerForChangeValue::OnCeaseRelevant(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
+{
+	OnTimerEnd.Unbind();
+	GetWorld()->GetTimerManager().ClearTimer(ChangeTimer);
+	Super::OnCeaseRelevant(OwnerComp, NodeMemory);
+}
+
+void UBTS_SetTimerForChangeValue::SetTimerForChangeValue(TWeakObjectPtr<UBehaviorTreeComponent> OwnerComp)
+{
+	UBehaviorTreeComponent* BehaviorTreeComponent = OwnerComp.Get();
+
+	if (BehaviorTreeComponent == nullptr)
+	{
+		return;
+	}
+
+	AAIController* AIController = BehaviorTreeComponent->GetAIOwner();
+
+	if (AIController == nullptr)
+	{
+		return;
+	}
+
+	UBlackboardComponent* BBComponent = AIController->GetBlackboardComponent();
+
+	if (BBComponent == nullptr)
+	{
+		return;
+	}
+	
+	switch (DataTypeToChange)
+	{
+	case BOOLTYPE :
+		BBComponent->SetValueAsBool(SelectedKey.SelectedKeyName, ChangeValBool);
+		break;
+	case FLOATTYPE:
+		BBComponent->SetValueAsFloat(SelectedKey.SelectedKeyName, ChangeValFloat);
+		break;
+	case INTTYPE:
+		BBComponent->SetValueAsInt(SelectedKey.SelectedKeyName, ChangeValInt);
+		break;
+	case VECTORTYPE:
+		BBComponent->SetValueAsVector(SelectedKey.SelectedKeyName, ChangeValVector);
+		break;
+	default :
+		break;
+	}
+}
+
+void UBTS_SetTimerForChangeValue::BroadcastEvent()
+{
+	if (OnTimerEnd.IsBound())
+	{
+		OnTimerEnd.Execute();
+	}
+}
---
+++ MoonU/Public/AI/Service/BTS_SetTimerForChangeValue.h
@@ -0,0 +1,62 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/BTService.h"
+#include "BTS_SetTimerForChangeValue.generated.h"
+
+/**
+ * 
+ */
+DECLARE_DELEGATE(FOnTimerEnd)
+
+UENUM(BlueprintType)
+enum EDataToChange
+{
+	BOOLTYPE,
+	INTTYPE,
+	FLOATTYPE,
+	VECTORTYPE
+};
+
+UCLASS()
+class MOONU_API UBTS_SetTimerForChangeValue : public UBTService
+{
+	GENERATED_BODY()
+
+	UBTS_SetTimerForChangeValue();
+
+	virtual void OnBecomeRelevant(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) override;
+
+	virtual void OnCeaseRelevant(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) override;
+
+	void SetTimerForChangeValue(TWeakObjectPtr<UBehaviorTreeComponent> OwnerComp);
+
+	void BroadcastEvent();
+
+	UPROPERTY(EditAnywhere, Category = TimerSetter)
+	FBlackboardKeySelector SelectedKey;
+	
+	UPROPERTY(EditAnywhere, Category = TimerSetter)
+	float TimerInterval;
+
+	UPROPERTY(EditAnywhere, Category = TimerSetter)
+	TEnumAsByte<EDataToChange> DataTypeToChange;
+
+	UPROPERTY(EditAnywhere, Category = TimerSetter)
+	float ChangeValFloat;
+
+	UPROPERTY(EditAnywhere, Category = TimerSetter)
+	int32 ChangeValInt;
+
+	UPROPERTY(EditAnywhere, Category = TimerSetter)
+	FVector ChangeValVector;
+	
+	UPROPERTY(EditAnywhere, Category = TimerSetter)
+	bool ChangeValBool;
+
+	FOnTimerEnd OnTimerEnd;
+	
+	FTimerHandle ChangeTimer;
+};
