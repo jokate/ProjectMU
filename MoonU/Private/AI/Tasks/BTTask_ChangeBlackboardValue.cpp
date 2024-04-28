1a57d0d1aadc841f92c035e37ccb7f056c204a4a
jokate <kkkk4017@naver.com>
2024-04-28 16:52:52 +0900

    MU_2024-04-28 / 16 : 52 SetValue Task 생성 완료.

---
+++ MoonU/Private/AI/Tasks/BTTask_ChangeBlackboardValue.cpp
@@ -0,0 +1,54 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AI/Tasks/BTTask_ChangeBlackboardValue.h"
+
+#include "AIController.h"
+#include "BehaviorTree/BlackboardComponent.h"
+
+EBTNodeResult::Type UBTTask_ChangeBlackboardValue::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
+{
+	AAIController* AIController = OwnerComp.GetAIOwner();
+
+	if (AIController == nullptr)
+	{
+		return EBTNodeResult::Failed;
+	}
+
+	UBlackboardComponent* BBComponent = AIController->GetBlackboardComponent();
+
+	if (BBComponent == nullptr)
+	{
+		return EBTNodeResult::Failed;
+	}
+	
+	switch (DataTypeToChange)
+	{
+	case BoolType :
+		BBComponent->SetValueAsBool(SelectedKey.SelectedKeyName, ChangeValBool);
+		break;
+	case FloatType:
+		BBComponent->SetValueAsFloat(SelectedKey.SelectedKeyName, ChangeValFloat);
+		break;
+	case IntType:
+		BBComponent->SetValueAsInt(SelectedKey.SelectedKeyName, ChangeValInt);
+		break;
+	case VectorType:
+		BBComponent->SetValueAsVector(SelectedKey.SelectedKeyName, ChangeValVector);
+		break;
+	case EnumType:
+		{
+			UEnum* Enum = FindObject<UEnum>(ANY_PACKAGE, *EnumName.ToString(), true);
+			if (!Enum)
+			{
+				break;
+			}
+
+			BBComponent->SetValueAsEnum(SelectedKey.SelectedKeyName, Enum->GetValueByName(EnumValue));
+		}
+	default :
+		break;
+	}
+
+	return EBTNodeResult::Succeeded;
+}
---
+++ MoonU/Public/AI/Tasks/BTTask_ChangeBlackboardValue.h
@@ -0,0 +1,44 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AI/AIEnum.h"
+#include "BehaviorTree/BTTaskNode.h"
+#include "BTTask_ChangeBlackboardValue.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MOONU_API UBTTask_ChangeBlackboardValue : public UBTTaskNode
+{
+	GENERATED_BODY()
+
+public :
+	virtual EBTNodeResult::Type ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) override;
+
+	UPROPERTY(EditAnywhere, Category = ValueSetter)
+	FBlackboardKeySelector SelectedKey;
+	
+	UPROPERTY(EditAnywhere, Category = ValueSetter)
+	TEnumAsByte<EBlackboardKeyData> DataTypeToChange;
+
+	UPROPERTY(EditAnywhere, Category = ValueSetter)
+	float ChangeValFloat;
+
+	UPROPERTY(EditAnywhere, Category = ValueSetter)
+	int32 ChangeValInt;
+
+	UPROPERTY(EditAnywhere, Category = ValueSetter)
+	FVector ChangeValVector;
+	
+	UPROPERTY(EditAnywhere, Category = ValueSetter)
+	bool ChangeValBool;
+
+	UPROPERTY(EditAnywhere, Category = ValueSetter)
+	FName EnumName;
+
+	UPROPERTY(EditAnywhere, Category = ValueSetter)
+	FName EnumValue;
+};
