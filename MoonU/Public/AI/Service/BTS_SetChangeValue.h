d2530952e82b694ce7eaeecf4156d4feccc14cd0
jokate <kkkk4017@naver.com>
2024-04-28 17:32:47 +0900

    MU_2024-04-28 / 17 : 32 StateChanger Service 추가 생성 완료.

---
+++ MoonU/Public/AI/Service/BTS_SetChangeValue.h
@@ -0,0 +1,51 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/BTService.h"
+#include "BTS_SetChangeValue.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MOONU_API UBTS_SetChangeValue : public UBTService
+{
+	GENERATED_BODY()
+
+	UBTS_SetChangeValue();
+
+	virtual void OnBecomeRelevant(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) override;
+
+	virtual void OnCeaseRelevant(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) override;
+	
+	void ChangeValue(UBehaviorTreeComponent& OwnerComp);
+	
+	UPROPERTY(EditAnywhere, Category = ValueSetter)
+	FBlackboardKeySelector SelectedKey;
+	
+	UPROPERTY(EditAnywhere, Category = ValueSetter)
+	TEnumAsByte<EBlackboardKeyData> DataTypeToChange;
+
+	UPROPERTY(EditAnywhere, Category = ValueSetter)
+	float ChangeValFloat;
+
+	UPROPERTY(EditAnywhere, Category = ValueSetter)
+	int32 ChangeValInt;
+
+	UPROPERTY(EditAnywhere, Category = ValueSetter)
+	FVector ChangeValVector;
+	
+	UPROPERTY(EditAnywhere, Category = ValueSetter)
+	bool ChangeValBool;
+
+	UPROPERTY(EditAnywhere, Category = ValueSetter)
+	FName EnumName;
+
+	UPROPERTY(EditAnywhere, Category = ValueSetter)
+	FName EnumValue;
+
+	UPROPERTY(EditAnywhere, Category = ActivationMode)
+	bool bBecomeOrCeased;
+};
