d05a361c34f11c9dd9f53bbf4d775ab93ff84999
jokate <kkkk4017@naver.com>
2024-05-01 15:40:32 +0900

    MU_2024-05-01 / 15 : 40 NavLink 생성 완료. (Smart NavLink)

---
+++ MoonU/Private/AI/NavLink/MUNavlink_EnemyAI.cpp
@@ -0,0 +1,40 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AI/NavLink/MUNavlink_EnemyAI.h"
+
+#include "GameFramework/Character.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "Kismet/GameplayStatics.h"
+
+
+// Sets default values
+AMUNavlink_EnemyAI::AMUNavlink_EnemyAI()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = false;
+	bSmartLinkIsRelevant = true;
+}
+
+// Called when the game starts or when spawned
+void AMUNavlink_EnemyAI::BeginPlay()
+{
+	Super::BeginPlay();
+	OnSmartLinkReached.AddDynamic(this, &AMUNavlink_EnemyAI::OnAIReachedToPoint);
+}
+
+void AMUNavlink_EnemyAI::OnAIReachedToPoint(AActor* MovingActor, const FVector& DestinationPoint)
+{
+	ACharacter* Character = Cast<ACharacter>(MovingActor);
+
+	if (Character == nullptr)
+	{
+		return;
+	}
+	
+	FVector OutVector;
+	UGameplayStatics::SuggestProjectileVelocity_CustomArc(this, OutVector, Character->GetActorLocation(), DestinationPoint);
+
+	Character->LaunchCharacter(OutVector, true, true);
+}
+
---
+++ MoonU/Public/AI/NavLink/MUNavlink_EnemyAI.h
@@ -0,0 +1,26 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Navigation/NavLinkProxy.h"
+#include "MUNavlink_EnemyAI.generated.h"
+
+UCLASS()
+class MOONU_API AMUNavlink_EnemyAI : public ANavLinkProxy
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	AMUNavlink_EnemyAI();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+
+	UFUNCTION()
+	virtual void OnAIReachedToPoint(AActor* MovingActor, const FVector& DestinationPoint);
+};
