e7045cba6a13404e69ccde331cd14be272b9cfed
jokate <kkkk4017@naver.com>
2024-05-07 16:37:35 +0900

    MU_2024-05-07 / 16 : 37 퍼펙트 닷지를 위한 태그 부착 Anim Notify State 추가.

---
+++ MoonU/Private/AnimNotify/AnimNotifyState_AddLooseTag.cpp
@@ -0,0 +1,51 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AnimNotify/AnimNotifyState_AddLooseTag.h"
+
+#include "AbilitySystemBlueprintLibrary.h"
+#include "AbilitySystemComponent.h"
+
+void UAnimNotifyState_AddLooseTag::NotifyBegin(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation,
+	float TotalDuration, const FAnimNotifyEventReference& EventReference)
+{
+	Super::NotifyBegin(MeshComp, Animation, TotalDuration, EventReference);
+
+	AActor* Owner = MeshComp->GetOwner();
+
+	if (Owner == nullptr)
+	{
+		return;
+	}
+
+	UAbilitySystemComponent* ASC = UAbilitySystemBlueprintLibrary::GetAbilitySystemComponent(Owner);
+
+	if (ASC == nullptr)
+	{
+		return;
+	}
+
+	ASC->AddLooseGameplayTag(NeedToAddTag);
+}
+
+void UAnimNotifyState_AddLooseTag::NotifyEnd(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation,
+	const FAnimNotifyEventReference& EventReference)
+{
+	AActor* Owner = MeshComp->GetOwner();
+
+	if (Owner == nullptr)
+	{
+		return;
+	}
+
+	UAbilitySystemComponent* ASC = UAbilitySystemBlueprintLibrary::GetAbilitySystemComponent(Owner);
+
+	if (ASC == nullptr)
+	{
+		return;
+	}
+
+	ASC->RemoveLooseGameplayTag(NeedToAddTag);
+	
+	Super::NotifyEnd(MeshComp, Animation, EventReference);
+}
---
+++ MoonU/Public/AnimNotify/AnimNotifyState_AddLooseTag.h
@@ -0,0 +1,24 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayTagContainer.h"
+#include "Animation/AnimNotifies/AnimNotifyState.h"
+#include "AnimNotifyState_AddLooseTag.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MOONU_API UAnimNotifyState_AddLooseTag : public UAnimNotifyState
+{
+	GENERATED_BODY()
+
+	virtual void NotifyBegin(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, float TotalDuration, const FAnimNotifyEventReference& EventReference) override;
+
+	virtual void NotifyEnd(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, const FAnimNotifyEventReference& EventReference) override;
+
+	UPROPERTY(EditAnywhere)
+	FGameplayTag NeedToAddTag;
+};
