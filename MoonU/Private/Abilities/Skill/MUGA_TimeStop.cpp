5e5ca410652e1193798b10d431135270d7a0f7ae
jokate <kkkk4017@naver.com>
2024-05-10 14:10:31 +0900

    MU_2024-05-10/ 14 : 10 인프라 코드 생성 완료.

--- MoonU/Private/Abilities/Skill/MUGA_TimeStop.cpp
+++ MoonU/Private/Abilities/Skill/MUGA_TimeStop.cpp
@@ -3,6 +3,9 @@
 
 #include "Abilities/Skill/MUGA_TimeStop.h"
 
+#include "AbilitySystemBlueprintLibrary.h"
+#include "Attribute/MUCharacterAttributeSet.h"
+
 UMUGA_TimeStop::UMUGA_TimeStop()
 {
 	InstancingPolicy = EGameplayAbilityInstancingPolicy::InstancedPerActor;
@@ -13,10 +16,30 @@ void UMUGA_TimeStop::ActivateAbility(const FGameplayAbilitySpecHandle Handle,
                                      const FGameplayEventData* TriggerEventData)
 {
 	Super::ActivateAbility(Handle, ActorInfo, ActivationInfo, TriggerEventData);
+
+	UAbilitySystemComponent* ASC = UAbilitySystemBlueprintLibrary::GetAbilitySystemComponent(ActorInfo->AvatarActor.Get());
+
+	if (ASC)
+	{
+		const UMUCharacterAttributeSet* CharacterAttribute = ASC->GetSet<UMUCharacterAttributeSet>();
+
+		if (CharacterAttribute)
+		{
+			GetWorld()->GetTimerManager().SetTimer(TimeStopHandle, this, &UMUGA_TimeStop::OnTimeFinished, CharacterAttribute->GetTimeStopDuration(), false);
+		}
+	}
 }
 
 void UMUGA_TimeStop::EndAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo,
 	const FGameplayAbilityActivationInfo ActivationInfo, bool bReplicateEndAbility, bool bWasCancelled)
 {
 	Super::EndAbility(Handle, ActorInfo, ActivationInfo, bReplicateEndAbility, bWasCancelled);
+}
+
+void UMUGA_TimeStop::OnTimeFinished()
+{
+	bool bReplicateEndAbility = true;
+	bool bWasCancelled = false;
+
+	EndAbility(CurrentSpecHandle, CurrentActorInfo, CurrentActivationInfo, bReplicateEndAbility, bWasCancelled);
 }
--- MoonU/Public/Abilities/Skill/MUGA_TimeStop.h
+++ MoonU/Public/Abilities/Skill/MUGA_TimeStop.h
@@ -20,4 +20,9 @@ public :
 	virtual void ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData) override;
 
 	virtual void EndAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, bool bReplicateEndAbility, bool bWasCancelled) override;
+
+	void OnTimeFinished();
+	
+	FTimerHandle TimeStopHandle;
 };
+
