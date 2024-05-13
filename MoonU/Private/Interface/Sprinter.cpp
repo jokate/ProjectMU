17a2bdcbd28d08f7ee21dfcda223ad84b7044d64
jokate <kkkk4017@naver.com>
2024-05-13 15:18:57 +0900

    MU_2024-05-13 / 15 : 18 미사용 인터페이스 제거 완료.

--- MoonU/Private/Interface/Sprinter.cpp
+++
@@ -1,7 +0,0 @@
-﻿// Fill out your copyright notice in the Description page of Project Settings.
-
-
-#include "Interface/Sprinter.h"
-
-
-// Add default functionality here for any ISprinter functions that are not pure virtual.
--- MoonU/Private/Interface/StreamableObject.cpp
+++
@@ -1,7 +0,0 @@
-﻿// Fill out your copyright notice in the Description page of Project Settings.
-
-
-#include "Interface/StreamableObject.h"
-
-
-// Add default functionality here for any IStreamableObject functions that are not pure virtual.
