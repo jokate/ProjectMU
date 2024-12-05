9c9fca359c7236659d83e805e2c8e4bdb86ee9b3
jokate <kkkk4017@naver.com>
2024-12-06 00:37:46 +0900

    MU_2024-12-06 / 12 : 37 타겟 판단을 위한 Decorator 선언 완료.

---
+++ MoonU/Private/AI/Decorator/BTD_TargetIsValid.cpp
@@ -0,0 +1,27 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AI/Decorator/BTD_TargetIsValid.h"
+
+#include "AIController.h"
+#include "Interface/MUEnemy.h"
+
+bool UBTD_TargetIsValid::CalculateRawConditionValue(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) const
+{
+	APawn* OwnerPawn = OwnerComp.GetAIOwner()->GetPawn();
+
+	if ( IsValid( OwnerPawn ) == false )
+	{
+		return false;
+	}
+
+	IMUEnemy* MUEnemy = Cast<IMUEnemy>(OwnerPawn);
+	
+	if ( MUEnemy == nullptr )
+	{
+		return false;
+	}
+
+	AActor* TargetActor = MUEnemy->GetActorTarget();
+	return IsValid(TargetActor);
+}
---
+++ MoonU/Public/AI/Decorator/BTD_TargetIsValid.h
@@ -0,0 +1,19 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/BTDecorator.h"
+#include "BTD_TargetIsValid.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MOONU_API UBTD_TargetIsValid : public UBTDecorator
+{
+	GENERATED_BODY()
+
+public :
+	virtual bool CalculateRawConditionValue(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) const override;
+};
