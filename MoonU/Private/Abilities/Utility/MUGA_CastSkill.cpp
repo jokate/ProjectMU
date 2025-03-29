ba84a4c57ed83de348d46458d8c8412e28d6e42f
jokate <kkkk4017@naver.com>
2025-03-29 17:00:56 +0900

    MU_2025-03-29 / 17 : 00 스킬 캐스팅 관련한 부분 초기 구현.

--- MoonU/Private/Abilities/Utility/MUGA_CastSkill.cpp
+++ MoonU/Private/Abilities/Utility/MUGA_CastSkill.cpp
@@ -3,11 +3,53 @@
 
 #include "Abilities/Utility/MUGA_CastSkill.h"
 
+#include "AbilitySystemBlueprintLibrary.h"
+#include "Interface/SkillManager.h"
+#include "Library/MUFunctionLibrary.h"
+
 void UMUGA_CastSkill::ActivateAbility(const FGameplayAbilitySpecHandle Handle,
-	const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo,
-	const FGameplayEventData* TriggerEventData)
+                                      const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo,
+                                      const FGameplayEventData* TriggerEventData)
 {
 	Super::ActivateAbility(Handle, ActorInfo, ActivationInfo, TriggerEventData);
+
+	AActor* OwnerActor = ActorInfo->AvatarActor.Get();
+
+	if ( IsValid(OwnerActor) == false )
+	{
+		EndAbility(Handle, ActorInfo, ActivationInfo, true, false);
+	}
+
+	ISkillManager* SkillManager = Cast<ISkillManager>(OwnerActor);
+
+	if ( SkillManager == nullptr )
+	{
+		EndAbility(Handle, ActorInfo, ActivationInfo, true, false);
+	}
+
+	FName SkillID = SkillManager->GetSkillIDBySlot( CastSkillSlotType );
+
+	FMUSkillData SkillData;
+	if ( UMUFunctionLibrary::GetSkillData( OwnerActor, SkillID, SkillData) == false)
+	{
+		EndAbility(Handle, ActorInfo, ActivationInfo, true, false);
+	}
+
+	switch (SkillData.CastingType)
+	{
+	case ESkillCastingType::Indicator :
+		{
+			// 스킬 캐스팅 용 Task 생성.
+			break;
+		}
+
+	case ESkillCastingType::Instant :
+		{
+			UAbilitySystemBlueprintLibrary::SendGameplayEventToActor(OwnerActor, SkillData.SkillGameplayTag, FGameplayEventData());
+			EndAbility(Handle, ActorInfo, ActivationInfo, true, false);
+			break;
+		}
+	}
 }
 
 void UMUGA_CastSkill::CancelAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo,
