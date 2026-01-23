2fb13116a0d2052484c01d2894f121adae04a56e
jokate <kkkk4017@naver.com>
2026-01-23 19:00:37 +0900

    MU_2026-01-23 / 18 : 58 월드 세팅 추가 / 아마 쓸일이 있을 듯 싶음.

---
+++ MoonU/Public/DevSetting/MUWorldSetting.h
@@ -0,0 +1,16 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/WorldSettings.h"
+#include "MUWorldSetting.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MOONU_API AMUWorldSetting : public AWorldSettings
+{
+	GENERATED_BODY()
+};
